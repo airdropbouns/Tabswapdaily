81ad426a3c291ab54aac57068fe63bdd14d52181
airdropbouns <114391241+airdropbouns@users.noreply.github.com>
2024-11-19 00:43:27 +0400

    Update tapswapDailytasks.js

--- tapswapDailytasks.js
+++ tapswapDailytasks.js
@@ -7,8 +7,8 @@
 // @match        https://app.tapswap.club/*
 // @icon         https://www.softportal.com/en/scr/1089/icons/icon_src.png
 // @grant        GM_webRequest
-// @downloadURL  https://raw.github.com/airdropbouns/Tabswapdaily/main/tapswapDailytasks.js
-// @updateURL    https://raw.github.com/airdropbouns/Tabswapdaily/main/tapswapDailytasks.js
+// @downloadURL  https://raw.githubusercontent.com/airdropbouns/Tabswapdaily/refs/heads/main/tapswapDailytasks.js
+// @updateURL    https://raw.githubusercontent.com/airdropbouns/Tabswapdaily/refs/heads/main/tapswapDailytasks.js
 // @homepage     https://github.com/airdropbouns/Tabswapdaily/
 // @require      https://code.jquery.com/jquery-3.7.1.min.js
 // ==/UserScript==
@@ -74,7 +74,7 @@ console.error = console.warn = console.info = console.debug = () => { };
       soal = soal.replace("`", "");
       console.log("Shahan Question : ---" + soal + "---");
     fetch(
-  "https://raw.githubusercontent.com/airdropbouns/TapswapDaily/main/list.json"
+  "https://raw.githubusercontent.com/airdropbouns/Tabswapdaily/refs/heads/main/list.json"
 ).then(function (response) {
   response.text().then(function (text) {
     storedText = text;
